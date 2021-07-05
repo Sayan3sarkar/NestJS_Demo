853e6c8c70522fa68ad5e82a7030fa69b2e84220
Sayan3sarkar <sayansarkar333@gmail.com>
2021-07-05 11:28:47 +0530

    Added generic return type to transform interceptor

--- src/transform.interceptor.ts
+++ src/transform.interceptor.ts
@@ -5,11 +5,12 @@ import {
   NestInterceptor,
 } from '@nestjs/common';
 import { classToPlain } from 'class-transformer';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
-  intercept(_ctx: ExecutionContext, next: CallHandler<any>) {
+  intercept(_ctx: ExecutionContext, next: CallHandler<any>): Observable<any> {
     return next.handle().pipe(map((data) => classToPlain(data)));
   }
 }
